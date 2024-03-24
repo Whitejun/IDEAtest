1a2fabcd9b3ad4611411dc5cfc27774907eb88a0
FelixceData <felixce@foxmail.com>
2024-03-24 20:13:32 +0800

    这是令狐冲的独孤九剑

---
+++ Java_01Module/src/com/test/java01/DuGUJiuJian.java
@@ -0,0 +1,11 @@
+package com.test.java01;
+
+public class DuGUJiuJian {
+    public static void main(String[] args) {
+        System.out.println("lhc的独孤九剑");
+        System.out.println("lhc的独孤九剑");
+        System.out.println("lhc的独孤九剑");
+        System.out.println("lhc的独孤九剑");
+        System.out.println("lhc的独孤九剑");
+    }
+}
